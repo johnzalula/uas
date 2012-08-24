3bac27a3f0e2968c0cf8e7eb5a0f1fdbadf5a15c
Eric Lembregts <eric@lembregts.eu>
2012-08-24 16:17:30 +0300

    Fix the views and add the radcheck

--- lib/LDAP.class.php
+++ lib/LDAP.class.php
@@ -4,6 +4,8 @@ class LDAP {
 
     private $_connection;
 
+    // When we create this object we need to provide the details
+    // Try to work with persistent connections?
     public function __construct($ldap_ip, $ldap_user, $ldap_password) {
         $this->_connection = ldap_connect($ldap_ip)
                 or die("Could not connect to ldap server");
@@ -11,18 +13,24 @@ class LDAP {
         ldap_bind($this->_connection, $ldap_user, $ldap_password);
     }
 
+    // Cleanup ofcourse
     public function __destruct() {
         ldap_close($this->_connection);
     }
 
     // needs an UAS user object
+    // a User object is represented by /lib/model/doctrine/User.class.php
     public function add_user($user) {
+        // Check if the user exists first, you never know ...
         if ($this->user_exists($user)) {
             $this->delete_user($user);
         }
         
+        // This is where we will insert the user in LDAP
         $udn = "uid=" . $user->getLogin() . ",ou=people,dc=mu,dc=edu,dc=et";
 
+        // Build the user in an array
+        // More information: 
         $user_to_add = array();
 
         $user_to_add["objectClass"][0] = "inetOrgPerson";
@@ -31,10 +39,12 @@ class LDAP {
         $user_to_add["uid"] = $user->getLogin();
         $user_to_add["cn"] = $user->getName() . " " . $user->getFathersName() . " " . $user->getGrandFathersName();
 
+        // Prevent empty sn's
         if (isset($user->getFathersName()) && $user->getFathersName() != "") {
             $user_to_add["sn"][0] = $user->getFathersName();
         }
 
+        // Prevent duplicate sn's
         if (isset($user->getGrandFathersName()) && $user->getGrandFathersName() != "") {
             if ($user->getGrandFathersName() != $user->getFathersName()) {
                 $user_to_add["sn"][1] = $user->getGrandFathersName();
@@ -46,6 +56,7 @@ class LDAP {
         $user_to_add["uidNumber"] = $user->getUid();
         $user_to_add["gidNumber"] = $user->getGid();
 
+        // Prepend {SSHA}
         if (isset($user->getUnixPassword()) && $user->getUnixPassword() != "") {
             $user_to_add["userPassword"] = "{SSHA}" . $user->getUnixPassword();
         } else {
@@ -58,6 +69,7 @@ class LDAP {
 
         $user_to_add["mail"] = $user->getEmailAddress();
 
+        // Prevent empty phone
         if (isset($user->getPhone()) && $user->getPhone() != "") {
             $user_to_add["homePhone"] = $user->getPhone();
         }
@@ -66,6 +78,7 @@ class LDAP {
     }
 
     public function delete_user($user) {
+        // Verify the existance first, you never know right
         if (!$this->user_exists($user)) {
             return false;
         }
@@ -75,6 +88,7 @@ class LDAP {
         return ldap_delete($this->_connection, $udn);
     }
 
+    // Update the password (for the updating of SSHA)
     public function update_password($user) {
         $udn = "uid=" . $user->getLogin() . ",ou=people,dc=mu,dc=edu,dc=et";
 
